0082a3969ddac7f7821de22e18e9499dde43d1a0
Jakhongir <jahongirbaxtiyorovich17@gmail.com>
2024-01-15 09:09:04 +0500

    Second commit

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
-from math import isclose
-from decimal import Decimal
-a = Decimal('0.1')
-b = Decimal('0.2')
-c = Decimal('0.3')
-
-print(a + b + c == Decimal('0.6'))
-
+def say_hi(name):
+    return f'Hi {name}'
